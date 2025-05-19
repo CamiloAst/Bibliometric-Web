bd07d595c1610df9c9f2d8dd4384217502dd90b6
Juan Camilo Astudillo <juanc.astudilloo@uqvirtual.edu.co>
2025-05-19 03:56:28 -0500

    cambio a las rutas de la api

--- fronted/src/pages/ContenidoRequerimiento.jsx
+++ fronted/src/pages/ContenidoRequerimiento.jsx
@@ -5,7 +5,7 @@ import GraficaImagen from "./GraficaImagen";
 const ContenidoRequerimiento = ({ archivo, requerimiento, datos }) => {
   const base = `${
     import.meta.env.VITE_API_URL
-  }/api/${archivo}/${requerimiento}`;
+  }/api/${requerimiento}/${archivo}`;
 
   switch (requerimiento) {
     case "estadisticas":
